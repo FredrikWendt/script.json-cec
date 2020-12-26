8b15ba0bcd79e40342631246346fcf5ca08559de
Fredrik Wendt <fredrik.wendt@proagile.se>
2020-12-27 00:11:06 +0100

    adds simple but working logging

--- script.py
+++ script.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
+
+import sys
+import os
+import logging
+logging.basicConfig(level=logging.DEBUG,
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M',
+        filename='/tmp/myapp.log',
+        filemode='wa')
+
+log = logging.getLogger(__name__)
+log.info("input: %s" % sys.argv)
+log.info("env: %s" % os.environ)
+
 import xbmc
 import urlparse
-import sys
 import time
+
 
 try:
         params = urlparse.parse_qs('&'.join(sys.argv[1:]))
         command = params.get('command',None)
 except:
         command = None
+
+log.info("Command is now %s" % command)
 
 if command and command[0] == 'activate':
         xbmc.executebuiltin('CECActivateSource')
@@ -24,3 +40,5 @@ elif command and command[0] == 'stop_and_standby':
                 xbmc.executebuiltin("PlayerControl(Stop)")
                 time.sleep(3)
         xbmc.executebuiltin('CECStandby')
+
+log.info("Terminating")
